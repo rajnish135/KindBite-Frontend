d796cc2908ff3b534475ab005b2f1d1af9f08ab5
rajnish135 <rajnishrajyan@gmail.com>
2025-06-24 11:55:31 +0530

    pushed vite.onfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-// https://vite.dev/config/
+// Emulate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    }
+  },
+  server: {
+    historyApiFallback: true
+  }
 })
